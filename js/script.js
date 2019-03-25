9eefb54f17f36e08f46b82311c0dc937322afe8e
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-03-25 23:35:42 +0100

    Add modals to index.html, sass and js script

--- js/script.js
+++ js/script.js
@@ -32,7 +32,6 @@ function inactiveGameDisplay() {
 
 // Buttons allowing player to chose a move
 var playerMoves = document.querySelectorAll(".player-move");
-
 for (var i = 0; i < playerMoves.length; i++) {
   playerMoves[i].addEventListener("click", function() {
     var playerMoveChoice = this.getAttribute("data-move");
@@ -131,3 +130,52 @@ newGame.addEventListener("click", function() {
       "</strong> wins";
   }
 });
+
+// remove "show" class from all modals
+function hideAllModals() {
+  for (var i = 0; i < modals.length; i++) {
+    modals[i].classList.remove("show");
+  }
+}
+
+// links opening modals
+// var modalLinks = document.querySelectorAll(".show-modal");
+
+// for (var i = 0; i < modalLinks.length; i++) {
+//   modalLinks[i].addEventListener("click", function() {
+//     var modalLinkId = this.getAttributeNode("href").value;
+//     showModal(event, modalLinkId);
+//   });
+// }
+
+// function opening modal
+var showModal = function(event, modalToBeShown) {
+  event.preventDefault();
+  hideAllModals();
+  document.querySelector(modalToBeShown).classList.add("show");
+  document.querySelector("#modal-overlay").classList.add("show");
+};
+
+// closing modals removing overlay display
+var hideModal = function(event) {
+  event.preventDefault();
+  document.querySelector("#modal-overlay").classList.remove("show");
+};
+
+var closeButtons = document.querySelectorAll(".modal .close");
+
+for (var i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].addEventListener("click", hideModal);
+}
+
+// closing by clicking overlay
+document.querySelector("#modal-overlay").addEventListener("click", hideModal);
+
+// stopping propagation in overlay
+var modals = document.querySelectorAll(".modal");
+
+for (var i = 0; i < modals.length; i++) {
+  modals[i].addEventListener("click", function(event) {
+    event.stopPropagation();
+  });
+}
